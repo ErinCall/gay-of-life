990739316eb5f820b27015ec9ef2242901eb8fd9
Erin Call <hello@erincall.com>
2021-06-04 16:03:04 -0700

    Randomize the initial state of the gameboard

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -3,14 +3,19 @@ import React from 'react';
 const STARTING_ROWS = 50;
 const STARTING_COLUMNS = 50;
 
+function initialCellState() {
+  return Math.random() < 1/50;
+}
+
 export default class Gameboard extends React.Component {
   constructor(props) {
     super(props);
 
+    const cells = Array(STARTING_ROWS).fill(null).map(() =>
+        Array(STARTING_COLUMNS).fill(null).map(initialCellState))
+
     this.state = {
-      cells: Array(STARTING_ROWS).fill(null).map(() =>
-        Array(STARTING_COLUMNS).fill(false)
-      ),
+      cells,
     };
   }
 
