46a2ee9a36296bba642744ebfde7dc2d8987a13b
Erin Call <hello@erincall.com>
2021-06-04 13:51:43 -0700

    Rough out a manually-created game field
    The squares all look the way I envisioned, so *hopefully* I'm done
    futzing around with the css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>The Game of Life</p>
+        <p>GNU John Conway</p>
       </header>
+      <div className="Game">
+        <div className="Row">
+          <div className="Cell">1</div>
+          <div className="Cell">2</div>
+          <div className="Cell">3</div>
+        </div>
+        <div className="Row">
+          <div className="Cell">4</div>
+          <div className="Cell">5</div>
+          <div className="Cell">6</div>
+        </div>
+        <div className="Row">
+          <div className="Cell">7</div>
+          <div className="Cell">8</div>
+          <div className="Cell">9</div>
+        </div>
+      </div>
     </div>
   );
 }
