f3580f285e46a5d96b6fcf8e9f49e95019be318e
Erin Call <hello@erincall.com>
2021-06-08 09:33:28 -0700

    Move cell-matrix logic back into the Gameboard
    I'm not certain this is actually better, since Gameboard.new() doesn't
    give you a fully-functional React component, but I think if we separate
    GOL-logic functions like `tick()` from state-management functions, we
    can avoid needing to write tests that want to look at both.

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,21 +1,115 @@
 import React from 'react';
-import CellMatrix from '../internal/cell-matrix';
+
+const DEFAULT_ROWS = 50;
+const DEFAULT_COLS = 50;
+const RANDOM_ALIVE_CHANCE = 1/50;
 
 export default class Gameboard extends React.Component {
   constructor(props) {
     super(props);
-    const matrix = new CellMatrix(props.rows, props.cols);
-    matrix.randomize();
+    const rows = props.rows || DEFAULT_ROWS;
+    const cols = props.cols || DEFAULT_COLS;
+
+    let cells = Array(rows).fill(null)
+        .map(() => Array(cols).fill(false));
+    cells = this.randomized(cells);
 
     this.state = {
-      matrix,
+      cells,
     };
+  }
+
+  randomized(cells) {
+    return cells.map(row =>
+      row.map(() => Math.random() < RANDOM_ALIVE_CHANCE)
+    );
+  }
+
+  tick() {
+    const newCells = this.state.cells.map(row => Array(row.length).fill(false));
+    for (let y = 0; y < this.state.cells.length; y++) {
+      for (let x = 0; x < this.state.cells[y].length; x++) {
+        const livingNeighbors = this.livingNeighbors(x, y);
+        if (livingNeighbors === 3 || (this.state.cells[y][x] && livingNeighbors === 2)) {
+          newCells[y][x] = true;
+        }
+      }
+    }
+
+    this.state = {
+      ...this.state,
+      cells: newCells,
+    };
+  }
+
+  livingNeighbors(x, y) {
+    return [
+      this.neighborUL(x,y), this.neighborU(x,y), this.neighborUR(x,y),
+      this.neighborL(x,y),                       this.neighborR(x,y),
+      this.neighborDL(x,y), this.neighborD(x,y), this.neighborDR(x,y),
+    ].reduce((count, alive) => alive ? count + 1 : count, 0);
+  }
+
+  neighborUL(x, y) {
+    if (x <= 0 || y <= 0) {
+      return false;
+    }
+    return this.state.cells[y-1][x-1];
+  }
+
+  neighborU(x, y) {
+    if (y <= 0) {
+      return false;
+    }
+    return this.state.cells[y-1][x];
+  }
+
+  neighborUR(x, y) {
+    if (y <= 0 || x >= this.state.cells[y].length-1) {
+      return false;
+    }
+    return this.state.cells[y-1][x+1];
+  }
+
+  neighborR(x, y) {
+    if (x >= this.state.cells[y].length-1) {
+      return false;
+    }
+    return this.state.cells[y][x+1];
+  }
+
+  neighborDR(x, y) {
+    if (y >= this.state.cells.length-1 || x >= this.state.cells[y].length-1) {
+      return false;
+    }
+    return this.state.cells[y+1][x+1];
+  }
+
+  neighborD(x, y) {
+    if (y >= this.state.cells.length-1) {
+      return false;
+    }
+    return this.state.cells[y+1][x];
+  }
+
+  neighborDL(x, y) {
+    if (y >= this.state.cells.length-1 || x <= 0) {
+      return false;
+    }
+    return this.state.cells[y+1][x-1];
+  }
+
+  neighborL(x, y) {
+    if (x <= 0) {
+      return false;
+    }
+    return this.state.cells[y][x-1];
   }
 
   render() {
     return (
       <div className="Gameboard">
-        {this.state.matrix.cells.map((row, rowNum) =>
+        {this.state.cells.map((row, rowNum) =>
           <div className="Row" key={rowNum}>
             {row.map((alive, colNum) =>
               <div className={`Cell ${alive ? 'alive' : 'dead'}`} key={colNum}></div>
--- src/components/Gameboard.test.js
+++ src/components/Gameboard.test.js
@@ -1,16 +1,173 @@
 import { screen, render } from '@testing-library/react';
 import Gameboard, {countLive} from './Gameboard';
 
-test('renders a gameboard', () => {
-  const {container} = render(<Gameboard rows={50} cols={50} />);
+test('renders the provided rows and cols', () => {
+  const {container} = render(<Gameboard rows={5} cols={5} />);
+  const rows = container.querySelectorAll('.Row');
+  expect(rows.length).toBe(5);
   const cells = container.querySelectorAll('.Cell');
-  expect(cells.length).toBe(2500);
+  expect(cells.length).toBe(25);
+});
+
+test('renders a default number of rows and cols', () => {
+  const {container} = render(<Gameboard />);
+  const rows = container.querySelectorAll('.Row');
+  expect(rows.length).not.toBe(0);
+  const cells = container.querySelectorAll('.Cell');
+  expect(cells.length).not.toBe(0);
 });
 
 test('randomizes the gameboard', () => {
   const {container} = render(<Gameboard rows={50} cols={50} />);
   const alive = container.querySelectorAll('.alive');
   const dead = container.querySelectorAll('.dead');
+
   expect(alive.length).toBeGreaterThan(0);
   expect(dead.length).toBeGreaterThan(0);
+});
+
+describe('neighbors', () => {
+  let gameboard;
+  beforeEach(() => {
+    gameboard = new Gameboard({rows: 4, cols: 4});
+    gameboard.state.cells = [
+      [ true, false, false, false],
+      [false, false, false, false],
+      [false, false,  true,  true],
+      [false, false, false,  true],
+    ];
+  });
+
+  test('livingNeighbors', () => {
+    expect(gameboard.livingNeighbors(0,0)).toBe(0);
+    expect(gameboard.livingNeighbors(1,0)).toBe(1);
+    expect(gameboard.livingNeighbors(0,1)).toBe(1);
+    expect(gameboard.livingNeighbors(2,1)).toBe(2);
+    expect(gameboard.livingNeighbors(2,3)).toBe(3);
+  });
+
+  test('up-left', () => {
+    expect(gameboard.neighborUL(1, 0)).toBe(false); // neighbor would be out of bounds
+    expect(gameboard.neighborUL(0, 1)).toBe(false); // neighbor would be out of bounds
+    expect(gameboard.neighborUL(1, 1)).toBe(true);
+    expect(gameboard.neighborUL(1, 2)).toBe(false); // neighbor is dead
+  });
+
+  test('up', () => {
+    expect(gameboard.neighborU(1, 0)).toBe(false); // neighbor would be out of bounds
+    expect(gameboard.neighborU(0, 1)).toBe(true);
+    expect(gameboard.neighborU(1, 1)).toBe(false); // neighbor is dead
+  });
+
+  test('up-right', () => {
+    expect(gameboard.neighborUR(2, 0)).toBe(false); // neighbor would be out of bounds
+    expect(gameboard.neighborUR(3, 1)).toBe(false); // neighbor would be out of bounds
+    expect(gameboard.neighborUR(1, 3)).toBe(true);
+    expect(gameboard.neighborUR(0, 3)).toBe(false); // neighbor is dead
+  });
+
+  test('right', () => {
+    expect(gameboard.neighborR(3, 1)).toBe(false); // neighbor would be out of bounds
+    expect(gameboard.neighborR(1, 2)).toBe(true);
+    expect(gameboard.neighborR(1, 1)).toBe(false); // neighbor is dead
+  });
+
+  test('down-right', () => {
+    expect(gameboard.neighborDR(3, 2)).toBe(false); // neighbor would be out of bounds
+    expect(gameboard.neighborDR(2, 3)).toBe(false); // neighbor would be out of bounds
+    expect(gameboard.neighborDR(2, 2)).toBe(true);
+    expect(gameboard.neighborDR(0, 2)).toBe(false); // neighbor is dead
+  });
+
+  test('down', () => {
+    expect(gameboard.neighborD(1, 3)).toBe(false); // neighbor would be out of bounds
+    expect(gameboard.neighborD(2, 1)).toBe(true);
+    expect(gameboard.neighborD(0, 2)).toBe(false); // neighbor is dead
+  });
+
+  test('down-left', () => {
+    expect(gameboard.neighborDL(0, 1)).toBe(false); // neighbor would be out of bounds
+    expect(gameboard.neighborDL(1, 3)).toBe(false); // neighbor would be out of bounds
+    expect(gameboard.neighborDL(3, 1)).toBe(true);
+    expect(gameboard.neighborDL(2, 1)).toBe(false); // neighbor is dead
+  });
+
+  test('left', () => {
+    expect(gameboard.neighborL(0, 1)).toBe(false); // neighbor would be out of bounds
+    expect(gameboard.neighborL(3, 2)).toBe(true);
+    expect(gameboard.neighborL(1, 1)).toBe(false); // neighbor is dead
+  });
+});
+
+describe('tick', () => {
+  test('life cannot thrive in a barren field', () => {
+    const gameboard = new Gameboard({rows: 3, cols: 3});
+    gameboard.state.cells = [
+      [false, false, false],
+      [false, false, false],
+      [false, false, false],
+    ];
+
+    gameboard.tick();
+    expect(gameboard.state.cells).toEqual([
+      [false, false, false],
+      [false, false, false],
+      [false, false, false],
+    ]);
+  });
+
+  test('to stand alone is to fall', () => {
+    const gameboard = new Gameboard({rows: 3, cols: 3});
+    gameboard.state.cells = [
+      [false,  true, false],
+      [false, false, false],
+      [false,  true, false],
+    ];
+
+    gameboard.tick();
+    expect(gameboard.state.cells).toEqual([
+      [false, false, false],
+      [false, false, false],
+      [false, false, false],
+    ]);
+  });
+
+  test('blinker', () => {
+    const gameboard = new Gameboard({rows: 3, cols: 3});
+    gameboard.state.cells = [
+      [false,  true, false],
+      [false,  true, false],
+      [false,  true, false],
+    ];
+
+    gameboard.tick();
+    expect(gameboard.state.cells).toEqual([
+      [false, false, false],
+      [ true,  true,  true],
+      [false, false, false],
+    ]);
+
+    gameboard.tick();
+    expect(gameboard.state.cells).toEqual([
+      [false,  true, false],
+      [false,  true, false],
+      [false,  true, false],
+    ]);
+  });
+
+  test('block', () => {
+    const gameboard = new Gameboard({rows: 3, cols: 3});
+    gameboard.state.cells = [
+      [false,  true,  true],
+      [false,  true,  true],
+      [false, false, false],
+    ];
+
+    gameboard.tick();
+    expect(gameboard.state.cells).toEqual([
+      [false,  true,  true],
+      [false,  true,  true],
+      [false, false, false],
+    ]);
+  });
 });
