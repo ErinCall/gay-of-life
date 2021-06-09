19af774c2a4fa85f7c6cdaf90d99b6e549036291
Erin Call <hello@erincall.com>
2021-06-09 16:46:55 -0700

    Move cells' pride flags into background images
    Having an <img> tag for each cell had a serious performance impact,
    since the images are around 1.5k. By making them background images, we
    only have to store a single instance of the image.

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,35 +1,41 @@
 import React from 'react';
+import {createUseStyles} from 'react-jss'
 
+const useStyles = createUseStyles({
+  Row: props => ({
+    display: 'flex',
+    height: `${100/props.cells.length}%`,
+    width: '100%',
+  }),
+  Alive: props => ({
+    backgroundImage: `url(${props.flag})`,
+    backgroundSize: ['100%', '100%'],
+  }),
+  Dead: props => ({
+    background: '#444444',
+  }),
+  Cell: props => ({
+    display: 'inline-block',
+    height: '100%',
+    width: `${100/props.cells[0].length}%`,
+  }),
+});
 
-export default class Gameboard extends React.Component {
-  Cell(alive, x, y) {
-    if (alive) {
-      return (<img
-        className="Cell alive"
-        key={x}
-        onClick={() => this.props.toggleCell(x, y)}
-        src={this.props.flag}
-        alt={`${this.props.flagName} pride flag`}
-      />);
-    } else {
-      return (<div
-        className="Cell dead"
-        key={x}
-        onClick={() => this.props.toggleCell(x, y)}
-      > </div> );
-    }
-  }
-  render() {
-    return (
-      <div className="Gameboard">
-        {this.props.cells.map((row, y) =>
-          <div className="Row" key={y}>
-            {row.map((alive, x) =>
-              this.Cell(alive, x, y)
-            )}
-          </div>
-        )}
-      </div>
-    );
-  }
+export default function Gameboard(props) {
+  const classes = useStyles(props);
+  return (
+    <div className="Gameboard">
+      {props.cells.map((row, y) =>
+        <div className={classes.Row} key={y}>
+          {row.map((alive, x) =>
+            <div
+              className={`${classes.Cell} ${alive ? classes.Alive : classes.Dead}`}
+              key={x}
+              onClick={() => props.toggleCell(x, y)}
+            ></div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
